aa99c19a85a14af9030aad995d19b6270dce05db
Chhabi Pachabhaiya <c.pachabhaiya@gmail.com>
2015-04-22 22:52:46 -0500

    Added all files.

---
+++ temp/theme-settings.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @file
+ * theme-settings.php
+ */
+
+/**
+ * Alter the theme-specific settings form.
+ */
+function drupalnepal_theme_form_system_theme_settings_alter(&$form, &$form_state) {
+
+}
